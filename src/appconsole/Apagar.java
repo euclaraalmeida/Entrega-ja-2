ee148f1e5153a88ff760cdb79353e13200956ecc
Mariana Sarinho <164221246+marisarinho@users.noreply.github.com>
2025-12-03 08:05:27 -0300

    Update Apagar.java

--- src/appconsole/Apagar.java
+++ src/appconsole/Apagar.java
@@ -7,17 +7,13 @@ public class Apagar {
     public Apagar() {
         try {
             System.out.println("Iniciando exclusão...");
-
- 
             Fachada.excluirEntregador("Davi");
-            
             System.out.println("Entregador 'Davi' excluído com sucesso (e entregas tratadas).");
 
         } catch (Exception e) {
             System.out.println("Erro ao apagar: " + e.getMessage());
         }
     }
-    
     public static void main(String[] args) {
         new Apagar();
     }
